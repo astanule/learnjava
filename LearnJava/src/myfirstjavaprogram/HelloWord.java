c8bc4fcd7ce618b32d163e80b48f008ab721a6f0
astanule <astanule@LOANRH-3RRNTK1.opentext.net>
2018-06-04 21:50:24 -0400

    first commit

---
+++ LearnJava/src/myfirstjavaprogram/HelloWord.java
@@ -0,0 +1,13 @@
+package myfirstjavaprogram;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World...");
+		System.out.print("Hello World...");
+		System.out.print("Here is my first java program");
+
+	}
+
+}
